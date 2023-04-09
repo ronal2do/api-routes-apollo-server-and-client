8ef80219bea02ce31669c671a44e45fc40747700
Ronaldo Lima <ronal2do@gmail.com>
2023-04-09 17:41:54 +0200

    update logo

--- recicla-server/src/components/PrimaryFeatures.tsx
+++ recicla-server/src/components/PrimaryFeatures.tsx
@@ -537,7 +537,7 @@ function FeaturesMobile() {
             ref={(ref) => ((slideRefs.current[featureIndex]) = ref)}
             className="w-full flex-none snap-center px-4 sm:px-6"
           >
-            <div className="relative transform overflow-hidden rounded-2xl bg-gray-800 px-5 py-6">
+            <div className="relative transform overflow-hidden rounded-2xl bg-blumine-800 px-5 py-6">
               <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
                 <CircleBackground
                   color="#13B5C8"
@@ -547,7 +547,7 @@ function FeaturesMobile() {
               <PhoneFrame className="relative mx-auto w-full max-w-[366px]">
                 <feature.screen />
               </PhoneFrame>
-              <div className="absolute inset-x-0 bottom-0 bg-gray-800/95 p-6 backdrop-blur sm:p-10">
+              <div className="absolute inset-x-0 bottom-0 bg-blumine-800/95 p-6 backdrop-blur sm:p-10">
                 <feature.icon className="h-8 w-8" />
                 <h3 className="mt-6 text-sm font-semibold text-white sm:text-lg">
                   {feature.name}
@@ -567,7 +567,7 @@ function FeaturesMobile() {
             key={featureIndex}
             className={clsx(
               'relative h-0.5 w-4 rounded-full',
-              featureIndex === activeIndex ? 'bg-gray-300' : 'bg-gray-500'
+              featureIndex === activeIndex ? 'bg-blumine-300' : 'bg-blumine-500'
             )}
             aria-label={`Go to slide ${featureIndex + 1}`}
             onClick={() => {
