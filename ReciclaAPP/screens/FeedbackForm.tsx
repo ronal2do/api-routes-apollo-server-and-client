b8cdc7622778bc7c983e02b7f4098364dad7e5ec
Ronaldo Lima <ronal2do@gmail.com>
2023-04-10 21:03:42 +0200

    update dependencies

--- ReciclaAPP/screens/FeedbackForm.tsx
+++ ReciclaAPP/screens/FeedbackForm.tsx
@@ -13,57 +13,9 @@ interface OtherProps {
   message: string;
 }
 
-// Aside: You may see InjectedFormikProps<OtherProps, FormValues> instead of what comes below in older code.. InjectedFormikProps was artifact of when Formik only exported a HoC. It is also less flexible as it MUST wrap all props (it passes them through).
-// const InnerForm = (props: OtherProps & FormikProps<FormValues>) => {
-//   const { handleSubmit, setFieldValue } = props;
-//   return (
-//     <View>
-//       <AwesomeInput color="green" value={props.values.content} label="content" onChange={text => setFieldValue('content', text)} />
-//       <Button
-//        label="Submit"
-//        backgroundColor="gray"
-//        onPress={handleSubmit as any}
-//      />
-//     </View>
-//   );
-// };
-
 const InnerForm = () => (
-  // <AwesomeInput color="green" value={props.values.content} label="content" onChange={text => setFieldValue('content', text)} />
   <AwesomeInput color="green" value="{props.values.content}" label="content" onChange={text => console.log('content', text)} />
 )
-
-// class C extends React.PureComponent<FormikProps<FormValues> & {}> {
-//   render() {
-//     const { handleSubmit } = this.props;
-//     return (
-//       <View
-//         style={{
-//           flex: 1,
-//           display: "flex",
-//           justifyContent: "center"
-//         }}
-//       >
-//         {/* <View>
-//           <Text style={{ fontSize: 30, marginBottom: 10 }}>Login</Text>
-//           <Field
-//             name="content"
-//             placeholder="Content"
-//             component={AwesomeInput}
-//             containerStyle={{ width: "100%" }}
-//             autoCapitalize="none"
-//           />
-//           <Button
-//             label="Submit"
-//             backgroundColor="gray"
-//             onPress={handleSubmit as any}
-//           />
-//         </View> */}
-//         <Text style={{ fontSize: 30, marginBottom: 10 }}>Login</Text>
-//       </View>
-//     );
-//   }
-// }
 
 export default withFormik<any, FormValues>({
   validationSchema: contentSchema,
