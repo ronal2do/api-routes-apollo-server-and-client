a2c9da882844fe95a48b81acb2b7ea5d9c8447bd
Ronaldo Lima <ronal2do@gmail.com>
2023-04-11 21:12:03 +0200

    damnn

--- recicla-server/src/lib/client.ts
+++ recicla-server/src/lib/client.ts
@@ -8,7 +8,7 @@ export const getClient = () => {
   if (!client || typeof window === "undefined") {
     client = new ApolloClient({
       link: new HttpLink({
-        uri: "http://localhost:3000/api/graphql",
+        uri: "https://reciclaapp.vercel.app/api/graphql",
       }),
       cache: new InMemoryCache(),
     });
